d0358286d52d6d8d11f4d23f45822e2a8172504f
Carrizo <agustin.carrizo@sovos.com>
2024-04-16 16:19:15 -0300

    Async chapter of the book

---
+++ books/NodeJS - Design Pattrerns/Callback Pattern - Error Propagation/index.js
@@ -0,0 +1,35 @@
+const { readFile } = require('fs')
+
+function readJSON(filename, callback) {
+    readFile(filename, 'utf-8', (err, data) => {
+        let parsed;
+        if (err) {
+            return callback(err)
+        }
+
+        try {
+            parsed = JSON.parse(data)
+        } catch (err) {
+            return callback(err)
+        }
+
+        callback(null, parsed)
+    })
+}
+
+function readJSONThrow(filename, callback) {
+    readFile(filename, 'utf-8', (err, data) => {
+        if (err) {
+            return callback(err)
+        }
+
+        callback(null, JSON.parse(data))
+    })
+}
+
+
+try {
+    readJSONThrow('test.json', (err) => console.log(err))
+} catch (error) {
+    console.log('This will NOT catch the JSON parsing exception')
+}
